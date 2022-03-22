9620efa61379d2fb2d4ee5fee30a7fa163d7da6d
xpack20 <96324926+almeidaa1@users.noreply.github.com>
2022-03-22 00:56:48 +0000

    Guiao1_Exercicios

--- 1º Ano/2º Semestre/Programação/FichasMoodle/Guiao1-Ponteiros/G1E8.c
+++ 1º Ano/2º Semestre/Programação/FichasMoodle/Guiao1-Ponteiros/G1E8.c
@@ -6,13 +6,13 @@ void main(void){ // Funçao main - Nao pedida no exercicio //
 	int tam;
 	printf("\nTamanho do seu array: ");
 	scanf("%d", &tam);
-	int tab[tam];
+	int tab[tam]; 
 	for(int i = 0; i < tam; i++) // Inicializa a tabela toda a zeros
 	 	*(tab+i) = 0;
 	for(int i = 1; i <= tam; i++) { 
 		printf("\nValor na posicao %d :",i);
 		scanf("%d", tab+i-1);
-		for(int x = 0; x < tam; x++) {
+		for(int x = 0; x < tam; x++) { // Printa toda a tabela
 			printf(" %d ", *(tab+x));
 		}
 	}
@@ -20,9 +20,9 @@ void main(void){ // Funçao main - Nao pedida no exercicio //
 }
 
 int maior_subida(int *tab, int dim) {
-	int maiorsub = *(tab+1)-*tab, pos = 1;
-		for(int i = 2; i < dim; i++) {
-			if(*(tab+i) - *(tab+(i-1)) >= maiorsub){
+	int maiorsub = *(tab+1)-*tab, pos = 0;
+		for(int i = 2; i < dim-1; i++) {
+			if(*(tab+i) > *(tab+i-1) && *(tab+i) - *(tab+i-1) > maiorsub){
 				maiorsub = *(tab+i) - *(tab+(i-1));
 				pos = i;
 			}
